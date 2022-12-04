9d11acec729b53da8ed62f64117a26fce6e26bfb
xoctavio <xavieroctavio91@hotmail.com>
2022-12-03 19:17:48 -0600

    Create variables
    - Created variables
    - Logged document.querySelector variables

---
+++ script.js
@@ -0,0 +1,13 @@
+// variables
+let hand = ["rock", "paper", "scissors"];
+
+let player = document.querySelector("#player");
+let cpu = document.querySelector("#cpu");
+let total = document.querySelector("#total");
+
+let playerPoints = 0;
+
+// check
+console.log(player);
+console.log(cpu);
+console.log(total);
